b6096eb5cea79ae2ead3aa07ee5968ff38b9b043
Anuragkumarsah <asah2307@gmail.com>
2023-06-19 19:44:46 +0530

    [MAJOR] removed predefined css and added home page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Ctscan from "./pages/Ctscan";
 import Mri from "./pages/Mri";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route exact path="/ctscan" element={<Ctscan />} />
         <Route path="/mri" element={<Mri />} />
       </Routes>
