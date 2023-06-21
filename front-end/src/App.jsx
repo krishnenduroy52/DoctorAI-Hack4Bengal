905d80f552365fc802b284ec74290f5bcc2c6e32
Anuragkumarsah <asah2307@gmail.com>
2023-06-21 11:48:35 +0530

    added navbar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,10 +5,12 @@ import Mri from "./pages/Mri";
 import Home from "./pages/Home";
 import Meet from "./pages/Meet";
 import Rooms from "./pages/Rooms";
+import Navbar from "../components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/ctscan" element={<Ctscan />} />
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,57 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import "../css/Home.css";
 
 const Home = () => {
-  const openMenu = () => {
-  }
+  
   return (
-    <div className="header">
-      {/*--------Navbar--------*/}
-      <div className="topNav">
-        {/* logo */}
-        <div className="logo">
-          <a href="">
-            <img src="" alt="" />
-            Logo
-          </a>
-        </div>
-        {/* menu */}
-        <ul className="links">
-          <li>
-            <a href="services">Services</a>
-          </li>
-          <li>
-            <a href="about">About</a>
-          </li>
-          <li>
-            <a href="contact-us">Contact Us</a>
-          </li>
-        </ul>
-        {/* signup */}
-        <a href="#" className="action_btn">
-          Sign In
-        </a>
-        <div className="toggle_btn" onClick={openMenu}>
-          <FontAwesomeIcon icon={faBarsStaggered} />
-        </div>
-      </div>
-      <div className="dropdown_menu">
-        <ul>
-          <li>
-            <a href="services">Services</a>
-          </li>
-          <li>
-            <a href="about">About</a>
-          </li>
-          <li>
-            <a href="contact-us">Contact Us</a>
-          </li>
-          <li><a href="#" className="action_btn">
-            Sign In
-          </a></li>
-        </ul>
-      </div>
+    <div className="home">
       {/*--------TopBody--------*/}
       <div className="topBody">{/* hero image */}</div>
       {/* Models of CT scan and MRI machines */}
