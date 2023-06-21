2bbb7f6f54f523307f4f961f1ce97a50ef48dfb3
Indrajit Pal <palindrajit10@gmail.com>
2023-06-21 19:18:33 +0530

    Nothing

--- front-end/src/pages/Rooms.jsx
+++ front-end/src/pages/Rooms.jsx
@@ -4,31 +4,31 @@ import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import "../css/Room.css";
 
 const Rooms = () => {
-  const appID = parseInt(import.meta.env.VITE_APP_ID);
-  const serverSecret = import.meta.env.VITE_APP_SERVER_SECRET;
-  const { roomId } = useParams();
-  const myMeeting = async (element) => {
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appID,
-      serverSecret,
-      roomId,
-      Date.now().toString(),
-      "Indrajit" //userName goes here
-    );
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
-    zp.joinRoom({
-      container: element,
-      scenario: {
-        mode: ZegoUIKitPrebuilt.VideoConference,
-      },
-    });
-  };
+    const appID = parseInt(import.meta.env.VITE_APP_ID);
+    const serverSecret = import.meta.env.VITE_APP_SERVER_SECRET;
+    const { roomId } = useParams();
+    const myMeeting = async (element) => {
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomId,
+            Date.now().toString(),
+            "Indrajit" //userName goes here
+        );
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        zp.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.VideoConference,
+            },
+        });
+    };
 
-  return (
-    <div className="room-page">
-      <div ref={myMeeting} style={{ height: "100%" }} />
-    </div>
-  );
+    return (
+        <div className="room-page">
+            <div ref={myMeeting} style={{ height: "100%" }} />
+        </div>
+    );
 };
 
 export default Rooms;
