060aba661efe7b58fe48fff39febc53091653a74
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-06-22 21:16:13 +0530

    sign up and log out with localstorage

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,28 +2,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
 import React, { useEffect, useRef, useState } from "react";
 import "../css/Navbar.css";
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const toogleBtn = useRef(null);
   const [toggleBtnIcon, setToggleBtnIcon] = useState(faBarsStaggered);
   const dropdownMenu = useRef(null);
 
-  const [isLogin, setIsLogin] = useState(false)
+  const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
-    const token = localStorage.getItem('doctor_ai_userID');
-    console.log(token)
+    const token = localStorage.getItem("doctor_ai_userID");
+    console.log(token);
     if (token) {
-      setIsLogin(true)
+      setIsLogin(true);
     } else {
-      setIsLogin(false)
+      setIsLogin(false);
     }
-  }, [isLogin])
+    console.log(isLogin);
+  }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('doctor_ai_userID')
-    setIsLogin(false)
-  }
+    localStorage.removeItem("doctor_ai_userID");
+    setIsLogin(false);
+  };
 
   const openMenu = () => {
     dropdownMenu.current.classList.toggle("open");
@@ -58,9 +59,15 @@ const Navbar = () => {
           </li>
         </ul>
         {/* signup */}
-        <a href="/signup" className="action_btn">
-          Sign In
-        </a>
+        {isLogin === false ? (
+          <Link to="/signup" className="action_btn">
+            Sign In
+          </Link>
+        ) : (
+          <Link onClick={handleLogout} className="action_btn">
+            Logout
+          </Link>
+        )}
         <div className="toggle_btn" ref={toogleBtn} onClick={openMenu}>
           <FontAwesomeIcon icon={toggleBtnIcon} />
         </div>
@@ -76,18 +83,19 @@ const Navbar = () => {
           <li className="dropdown_links">
             <a href="contact-us">Contact Us</a>
           </li>
-          {!isLogin ? <li>
-            <a href="#" className="action_btn">
-              Sign In
-            </a>
-          </li> :
-            <li onClick={handleLogout}>
-              <div className="action_btn">
-                Logout
-              </div>
+          {isLogin === false ? (
+            <li className="dropdown_links">
+              <Link to="/signup" className="action_btn">
+                Sign In
+              </Link>
             </li>
-          }
-
+          ) : (
+            <li className="dropdown_links">
+              <Link onClick={handleLogout} className="action_btn">
+                Log Out
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
