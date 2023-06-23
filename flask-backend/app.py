fb8c7b80068b9974a823538acf5b8902c7487d76
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-06-24 00:30:00 +0530

    flask clean up

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.densenet import preprocess_input, decode_predictions
+from tensorflow.keras.applications.densenet import preprocess_input
 from tensorflow.keras.applications.vgg16 import preprocess_input as preprocess_input_mri
-from tensorflow.keras.applications.resnet50 import preprocess_input as preprocess_input_resnet
 import numpy as np
 import os
 from flask_cors import CORS
